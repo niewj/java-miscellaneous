902599192d87eefe2f63e33b9778834292702bc8
weijun.nie <weijun.nie@msxf.com>
2017-10-27 15:32:27 +0800

    Initial commit

---
+++ concurrent/src/main/java/com/niewj/basic/concurrent/bruceEckel/constructorSafe/StaticIDField.java
@@ -0,0 +1,10 @@
+package com.niewj.basic.concurrent.bruceEckel.constructorSafe;// StaticIDField.java
+
+public class StaticIDField implements HasID {
+    private static int counter = 0;
+    private int id = counter++;
+
+    public int getID() {
+        return id;
+    }
+}
---
+++ concurrent/src/main/java/com/niewj/basic/concurrent/patterns/builder2/Builder.java
@@ -0,0 +1,7 @@
+package com.niewj.basic.concurrent.patterns.builder2;
+
+/**
+ * Created by weijun.nie on 2017/10/27.
+ */
+public class Builder {
+}
