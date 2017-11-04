713d956fbf4411a7b3879ff4e8dbcae14af927e9
niewj <dotjar@163.com>
2017-11-04 11:22:02 +0800

    暂时去掉hadoop/hbase 依赖

--- webdk/src/main/java/com/niewj/common/util/SlowLogUtils.java
+++ webdk/src/main/java/com/niewj/common/util/SlowLogUtils.java
@@ -1,7 +1,7 @@
 package com.niewj.common.util;
 
 import com.google.gson.Gson;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
